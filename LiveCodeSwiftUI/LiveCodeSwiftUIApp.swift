d17f9b1651e70d22edbb0b2bebaf05c8aee94ed2
JanaSyntax <jana.jansen@syntax-institut.de>
2024-12-16 10:38:55 +0100

    Initial Commit

---
+++ LiveCodeSwiftUI/LiveCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCodeSwiftUIApp.swift
+//  LiveCodeSwiftUI
+//
+//  Created by Jana Jansen on 16.12.24.
+//
+
+import SwiftUI
+
+@main
+struct LiveCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
