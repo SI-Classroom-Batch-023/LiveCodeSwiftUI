643a5af289a96a0d4f7d14fd07da8d9f287f3548
JanaSyntax <jana.jansen@syntax-institut.de>
2024-12-16 10:40:08 +0100

    update greeting text

--- LiveCodeSwiftUI/ContentView.swift
+++ LiveCodeSwiftUI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Herzlich Willkommen Batch 23!")
         }
         .padding()
     }
